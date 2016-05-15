2eaad6060ae91c2bcbc91fbd572d7cb5a0dccde1
Krzysztof <krzysztof.kapitan@gmail.com>
2016-05-15 18:06:02 +0200

    Add api version constraints (+ specs)

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,17 @@ module Smarthouse
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helpe = false
+    end
+
+    config.autoload_paths += %W(\#{config.root}/lib)
   end
 end
