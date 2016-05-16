46f4868efdc92a6610fcf3593fc851fb9794a608
Krzysztof <krzysztof.kapitan@gmail.com>
2016-05-16 22:27:22 +0200

    POST /users endpoint (+specs and specs refactor)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,21 @@ class Api::V1::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    render :template =>"/api/v1/users/show.json.jbuilder", :status=> 200, :formats => [:json]
+    render :template =>"/api/v1/users/show.json.jbuilder", :status => 200, :formats => [:json]
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :template =>"/api/v1/users/create.json.jbuilder", :status => 201, :formats => [:json]
+    else
+      render :template =>"/api/v1/users/errors.json.jbuilder", :status => 422, :formats => [:json]
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -11,11 +11,48 @@ describe Api::V1::UsersController do
     end
 
     it "returns the information about a reporter on hash" do
-      user_response = JSON.parse(response.body, symbolize_names: true)
+      user_response = json_response
       expect(user_response[:email]).to eql @user.email
     end
 
     it { should respond_with 200 }
+  end
+
+  describe "POST #create" do
+
+    context "When request results in success" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, user: @user_attributes, format: :json
+      end
+
+      it "returns the information about created object" do
+        user_response = json_response
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "When request results in error" do
+
+      before(:each) do
+        @invalid_user_attributes = FactoryGirl.attributes_for :user
+        @invalid_user_attributes[:email] = "invalid.email"
+
+        post :create, user: @invalid_user_attributes, format: :json
+      end
+
+      it "returns information about errors" do
+        user_response = json_response
+        expect(user_response).to have_key(:errors)
+      end
+
+      it { should respond_with 422 }
+    end
+
+
+
   end
 
 end
