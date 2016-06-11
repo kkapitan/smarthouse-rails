5e237dfab1805e40b989df4f85901a15092a827d
Jan Badura <jan.badura@op.pl>
2016-06-11 22:08:07 +0200

    Change cron command format -> remove / "

--- app/helpers/api/v1/actions_helper.rb
+++ app/helpers/api/v1/actions_helper.rb
@@ -22,7 +22,7 @@ module Api::V1::ActionsHelper
     state = Action.states[action.state].to_i
     as_id = action.action_subject_id
 
-    task = "\"* * * * #{days} expr `date +(%H*60+%M) | bc` \% #{time} > /dev/null || [ `date +(%H*60+%M) | bc` -lt #{start} ] || [ `date +(%H*60+%M) | bc` -gt #{endd} ] || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}\""
+    task = "* * * * #{days} expr `date +(%H*60+%M) | bc` \% #{time} > /dev/null || [ `date +(%H*60+%M) | bc` -lt #{start} ] || [ `date +(%H*60+%M) | bc` -gt #{endd} ] || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}"
     system('sudo', '/addCronTask', task)
 
   end
