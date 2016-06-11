ee4b5dac0d8c1142cd6df32632244c787b62505b
Jan Badura <jan.badura@op.pl>
2016-06-11 20:43:34 +0200

    Add add_daily_cron_task

--- app/helpers/api/v1/actions_helper.rb
+++ app/helpers/api/v1/actions_helper.rb
@@ -12,19 +12,31 @@ module Api::V1::ActionsHelper
 
   private
   def add_daily_cron_task(trigger, action)
+    time = trigger.minutes + trigger.hours * 60
+    days = trigger.week_days.to_s.tr('[]','')
+    start = Time.at(trigger.start_hour).hour*60 + Time.at(trigger.start_hour).min
+    endd = Time.at(trigger.finish_hour)*60 + Time.at(trigger.finish_hour).min
+
+    conf = action.configuration
+    state = Action.states[action.state].to_i
+    as_id = action.action_subject_id
+
+    task = "\"* * * * #{days} expr `date +(%H*60+%M) | bc` \% #{time} > /dev/null || [ `date +(%H*60+%M) | bc` -lt #{start} ] || [ `date +(%H*60+%M) | bc` -gt #{endd} ] || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}\""
+    system('sudo', '/addCronTask', task)
 
   end
 
   def add_weakly_cron_task(trigger, action)
     weeks = trigger.weeks
     days = trigger.week_days.to_s.tr('[]','')
-    hour = trigger.day_hour
+    hour = Time.at(trigger.day_hour).hour
+    minutes = Time.at(trigger.day_hour).min
 
     conf = action.configuration
     state = Action.states[action.state].to_i
     as_id = action.action_subject_id
 
-    task = "\"* #{hour} * * #{days} expr `date +\%W` \% weeks > /dev/null || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}\""
+    task = "\"#{minutes} #{hour} * * #{days} expr `date +\%W` \% #{weeks} > /dev/null || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}\""
     system('sudo', '/addCronTask', task)
   end
 
