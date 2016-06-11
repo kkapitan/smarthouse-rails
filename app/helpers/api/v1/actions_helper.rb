eb479df1806ee1ef561918bba727d1d51f288ace
Jan Badura <jan.badura@op.pl>
2016-06-11 21:30:39 +0200

    Fix days format

--- app/helpers/api/v1/actions_helper.rb
+++ app/helpers/api/v1/actions_helper.rb
@@ -30,7 +30,7 @@ module Api::V1::ActionsHelper
   def add_weakly_cron_task(trigger, action)
     print "Robie KURWA!!!"
     weeks = trigger.weeks
-    days = trigger.week_days.to_s.tr('[]','')
+    days = trigger.week_days.to_s.tr('[]"','').tr(' ', '')
     hour = Time.at(trigger.day_hour).hour
     minutes = Time.at(trigger.day_hour).min
 
@@ -38,7 +38,7 @@ module Api::V1::ActionsHelper
     state = Action.states[action.state].to_i
     as_id = action.action_subject_id
 
-    task = "\"#{minutes} #{hour} * * #{days} expr `date +\%W` \% #{weeks} > /dev/null || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}\""
+    task = "#{minutes} #{hour} * * #{days} expr `date +\%W` \% #{weeks} > /dev/null || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}
     system('sudo', '/addCronTask', task)
   end
 
