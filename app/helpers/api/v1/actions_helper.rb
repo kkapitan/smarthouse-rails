45c9694e07b22e71920f5a5dc3390ccec2f89287
Jan Badura <jan.badura@op.pl>
2016-06-12 02:02:27 +0200

    Escape ( and % in in task

--- app/helpers/api/v1/actions_helper.rb
+++ app/helpers/api/v1/actions_helper.rb
@@ -22,13 +22,13 @@ module Api::V1::ActionsHelper
     state = Action.states[action.state].to_i
     as_id = action.action_subject_id
 
-    task = "* * * * #{days} expr `date +(%H*60+%M) | bc` \% #{time} > /dev/null || [ `date +(%H*60+%M) | bc` -lt #{start} ] || [ `date +(%H*60+%M) | bc` -gt #{endd} ] || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}"
+    task = "* * * * #{days} expr `date +\\(\\%H*60+\\%M\\) | bc` \\% #{time} > /dev/null || [ `date +\\(\\%H*60+\\%M\\) | bc` -lt #{start} ] || [ `date +\\(\\%H*60+\\%M\\) | bc` -gt #{endd} ] || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}"
     system('sudo', '/addCronTask', task)
 
   end
 
   def add_weakly_cron_task(trigger, action)
-    print "Robie KURWA!!!"
+    print "Robie 2!"
     weeks = trigger.weeks
     days = trigger.week_days.map{|x| x.to_i + 1}.to_s.tr('[]"','').tr(' ', '')
     hour = Time.at(trigger.day_hour).hour + 180
@@ -38,7 +38,7 @@ module Api::V1::ActionsHelper
     state = Action.states[action.state].to_i
     as_id = action.action_subject_id
 
-    task = "#{minutes} #{hour} * * #{days} expr `date +\%W` \% #{weeks} > /dev/null || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}"
+    task = "#{minutes} #{hour} * * #{days} expr `date +\\%W` \\% #{weeks} > /dev/null || /cronTask #{conf} #{state} #{as_id} #ID=#{action.id}"
     system('sudo', '/addCronTask', task)
   end
 
